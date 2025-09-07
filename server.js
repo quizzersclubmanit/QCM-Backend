6c7e06f2dcac51b31445b42081b4cdf5d9916c67
Quizzers' Club NIT Bhopal <95757569+quizzersclubmanit@users.noreply.github.com>
2025-09-07 20:43:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://www.quizzersclub.in/' ,
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://www.quizzersclub.in' ,
   credentials: true
 }));
 app.use(express.json());
