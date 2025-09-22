58500e7bf300a6c51a7cef7c71431095efbb70b9
Quizzers' Club NIT Bhopal <95757569+quizzersclubmanit@users.noreply.github.com>
2025-09-22 18:49:46 +0530

    get user bugfix

--- server.js
+++ server.js
@@ -49,22 +49,36 @@ const allowedOrigins = [
 ];
 
 // CORS configuration
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
     
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true,
+//   allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token', 'token', 'X-Requested-With', 'Accept', 'Origin'],
+//   exposedHeaders: ['set-cookie'],
+//   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 200
+// };
+const corsOptions = {
+  origin: allowedOrigins, 
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token', 'token', 'X-Requested-With', 'Accept', 'Origin'],
-  exposedHeaders: ['set-cookie'],
-  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-  preflightContinue: false,
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "Accept",
+    "Origin",
+    "Cache-Control"
+  ],
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
   optionsSuccessStatus: 200
 };
 
